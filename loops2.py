c2cf1dff91851e76dcb7f72fb7b2bbf69246a51d
taruni117 <180143900+taruni117@users.noreply.github.com>
2026-03-03 23:14:56 +0530

    Add files via upload

---
+++ loops2.py
@@ -0,0 +1,93 @@
+#range()
+#start,stop,step
+#start
+'''for i in range(10):
+    print(i)'''
+#stop
+'''for i in range(5,15):
+    print(i)'''
+#step
+'''for i in range(0,50,5):
+    print(i)'''
+#start-stop-step
+'''for i in range(0,20,2):
+    print(i)'''
+#task
+#marks and grades
+'''while True:
+    marks=int(input("enter the marks"))
+    if marks in range(91,101):
+        print("grade-A")
+    elif marks in range(81,91):
+        print("grade-B")
+    elif marks in range(71,81):
+        print("grade-c")
+    elif marks in range(50,71):
+        print("grade-D")
+    else:
+        print("study well")'''
+
+#break
+'''a=10
+while a<1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1
+    if a==5:
+        break'''
+
+'''for i in range(20):
+    print(i)'''
+
+'''for i in range(20):
+    if i==11:
+        break
+    print(i)'''
+#continue()
+'''a=30
+while a>5:
+    print(a)
+    a=a-1'''
+'''a=30
+while a>5:
+    a=a-1
+    print(a)'''
+
+'''a=30
+while a>5:
+    print(a)
+    a=a-1
+    if a==15:
+        continue'''
+'''a=30
+while a>5:
+    a=a-1
+    if a==15:
+        continue
+    print(a)'''
+'''for i in range(25):
+    print(i)'''
+'''for i in range(25):
+    if i==20:
+        continue
+    print(i)'''
+'''a="course"
+for i in a:
+    if i=="r":
+        continue
+    print(i)'''
+#pass
+'''a=30
+while a>20:
+    print(a)
+    a=a-1'''
+
+a=30
+while a>20:
+    print(a)
+    a=a-1
+    if a==15:
+        pass
