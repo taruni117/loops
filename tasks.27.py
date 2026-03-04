c110a19d5d2c681afd5da879a311c630a7428380
taruni117 <180143900+taruni117@users.noreply.github.com>
2026-03-04 17:54:53 +0530

    Add files via upload

---
+++ tasks.27.py
@@ -0,0 +1,40 @@
+#task
+'''name=input("enter your name")
+if name=="taruni":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+
+'''a=input("enter your name")
+names=["harika","simha","adithya","mahesh","taruni"]
+if a in names:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+#nested if[social media]
+'''a=input("enter your username")
+b=int(input("enter your password"))
+if a=="taruni":
+    if b==1234:
+      print("valid")
+else:
+    print("invalid credentials")'''
+
+'''username=input("enter the username")
+password=int(input("enter the password"))
+if username=="taruni" and password==1234:
+        print("valid")
+else:
+    print("invalid")'''
+#bakery
+cake_price=int(input("enter the price"))
+if cake_price==1200:
+    print("redvelvet cake")
+elif  cake_price==1000:
+    print("chocolate cake")
+elif cake_price==800:
+    print("butter scotch cake")
+elif cake_price==600:
+    print("chop almond cake")
+else:
+    print("sorry cake not available")
---
+++ tasktable.py
@@ -0,0 +1,38 @@
+#task table
+'''num=int(input("enter you table"))
+for i in range(1,21):
+    print(num,"x",i,"=",num*i)'''
+
+#student profile
+'''n1=(input("enter your name"))
+n2=int(input("enter your mobile number"))
+n3=(input("enter your mail-id"))
+n4=(input("enter your college name"))
+n5=(input("enter your branch"))
+print("n1 is",n1)
+print("n2 is",n2)
+print("n3 is",n3)
+print("n4 is",n4)
+print("n5 is",n5)'''
+
+#marks analyzer
+'''n=int(input("enter the students count"))
+marks=[]
+
+for i in range(n):
+    mark=int(input("enter the marks"))
+    marks.append(mark)
+
+for j in marks:
+    print(j)
+heighest_marks=max(marks)
+lowest_marks=min(marks)
+total_marks=sum(marks)
+average=total_marks/n
+print("marks report......")
+print("heighest marks",heighest_marks)
+print("lowest marks",lowest_marks)
+print("total marks",total_marks)
+print("average",average)'''
+
+#caluclate hostel fee+couse fee
